3131209f663613612aa40ace2a82e60d18b38864
Murat Okçu <mrtkcu0101@gmail.com>
2024-11-28 14:13:00 +0300

    login&success pages

---
+++ vite-project/src/Success.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Success() {
+  return (
+    <div>
+      <h1>Login Successful!</h1>
+      <p>Welcome to the application.</p>
+    </div>
+  );
+}
